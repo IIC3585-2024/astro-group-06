1a2e142141eb18b3c109eac4b388e18d18b66b28
aerotecnia99 <valentinacamph@uc.cl>
2024-06-26 04:11:02 -0400

    feat(): show series index

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = ({ cookies, redirect }) => {
+  const accessToken = cookies.get("sb-access-token");
+  const refreshToken = cookies.get("sb-refresh-token");
+
+  return (
+    <div className="navbar bg-neutral text-neutral-content flex flex-col md:flex-row gap-5 mb-5">
+      <a href="/">Home</a>
+      {(!accessToken || !refreshToken) ? (
+        <>
+          <a href="/signin">Login</a>
+          <a href="/register">Register</a>
+        </>
+      ) : (
+        <>
+          <a href="/add-serie">Add New Series</a>
+          <form action="/api/auth/signout">
+            <button type="submit">Logout</button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/SeriesList.tsx
@@ -0,0 +1,34 @@
+import SeriesCard from './SeriesCard';
+
+interface SeriesItem {
+  id: number;
+  name: string;
+  categories: string[];
+  created_at: string;
+  streaming_service: string;
+  description: string;
+  category: string;
+  average_rating: number;
+  seasons: number;
+  episodes_per_season: number;
+  image_url: string;
+}
+
+interface SeriesListProps {
+  series?: SeriesItem[];
+}
+
+const SeriesList = ({series = []}: SeriesListProps) => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {series.map((seriesItem) => (
+          <SeriesCard key={seriesItem.id} series={seriesItem} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SeriesList;
+
